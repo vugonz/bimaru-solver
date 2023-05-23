0646fb1014ec110513fec273efd4ab3df608ac7c
vugonz <goncalooazevedo@outlook.com>
2023-05-23 05:31:21 +0100

    i am crying x2

--- bimaru/bimaru.py
+++ bimaru/bimaru.py
@@ -24,6 +24,9 @@ from search import (
 class InvalidCell(Exception):
     ...
 
+class InvalidShip(Exception):
+    ...
+
 
 class BimaruState:
     state_id = 0
@@ -46,6 +49,7 @@ class Board:
                  cols: list,
                  hints: list,
                  ships: list,
+                 has_hints: bool,
                  ):
         self.table = table
         self.rows = rows
@@ -53,56 +57,156 @@ class Board:
         self.hints = hints
         self.ships = ships
         self.is_valid = True
-        self.has_hints = True
+        self.has_hints = has_hints
+
+    def action(self, row, col, operation, size=0):
+        new_board = self.board_copy()
+
+        try:
+            if operation == "v":
+                new_board.add_vertical_ship(row, col, size)
+            elif operation == "h":
+                new_board.add_horizontal_ship(row, col, size)
+            elif operation == "w":
+                new_board.add_water(row, col)
+            else:
+                new_board.add_single_ship(row, col)
+        except (InvalidCell, InvalidShip):
+            new_board.is_valid = False
+
+        return new_board
+
+    def is_valid_action(self, row, col, operation, size=0):
+        if operation == "v":
+            ...
+        elif operation == "h":
+            ...
+        elif operation == "w":
+            ...
+        else:
+            ...
+
 
     def get_valid_actions(self):
         if self.has_hints:
-            return self.get_hints_actions()
+            return self.get_hint_actions(*self.hints.pop())
 
-        pass
+        actions = []
+        for i in range(10):
+            for j in range(10):
+                if self.table[i][j] == "*" and self.rows[i] and self.cols[j]:
+                    #actions.append((i, j, "w"))
+                    if self.ships[0]:
+                        actions.append((i, j, "c"))
+                    for s in range(2, 5):
+                        if self.ships[s - 1]:
+                            actions.append((i, j, "v", s))
+                            actions.append((i, j, "h", s))
 
-    def get_hint_actions(self):
-        ...
+        return actions
+
+    def get_hint_actions(self, row: int, col: int, letter: str):
+        actions = []
+
+        if letter == "L":
+            for i in range(2, 5):
+                if self.ships[i - 1]:
+                    actions.append((row, col, "h", i))
+                    # remove hints to this ship
+                    for j in range(len(self.hints)):
+                        for x in range(1, i):
+                            if self.hints[j][0] == row:
+                                self.hints.pop(j)
+        elif letter == "T":
+            for i in range(2, 5):
+                if self.ships[i - 1]:
+                    actions.append((row, col, "v", i))
+        elif letter == "R":
+            for i in range(2, 5):
+                if self.ships[i - 1]:
+                    actions.append((row, col - i + 1, "h", i))
+        elif letter == "B":
+            for i in range(2, 5):
+                if self.ships[i - 1]:
+                    actions.append((row - i + 1, col, "v", i))
+        elif letter == "M":
+            if self.ships[2]:
+                actions.append((row - 1, col, "v", 3))
+                actions.append((row, col - 1, "h", 3))
+            if self.ships[3]:
+                actions.append((row - 1, col, "v", 4))
+                actions.append((row - 2, col, "v", 4))
+                actions.append((row, col - 1, "h", 4))
+                actions.append((row, col - 2, "h", 4))
+
+        return actions
+
+    def is_complete(self):
+        print(self)
+        print("\n")
+        return all(x == 0 for x in self.ships + self.cols + self.rows)
 
     #############
     # Actions
     #############
     def add_horizontal_ship(self, row, col, size):
+        # ship will fall out
+        if col + size > 10:
+            raise InvalidShip
+
+        # no space in row
+        if self.rows[row] < size:
+            raise InvalidShip
         self.rows[row] -= size
+
+        # no space in columns
+        for i in range(col, col + size):
+            if not self.cols[i]:
+                raise InvalidShip
+            self.cols[i] -= 1
+
         self.ships[size - 1] -= 1
 
         self.add_left_cell(row, col)
-        self.cols[col] -= 1
 
         # add middle
         i = 0
         for i in range(1, size - 1):
             self.add_middle_horizontal_cell(row, col + i)
-            self.cols[col + i] -= 1
 
         self.add_right_cell(row, col + i + 1)
-        self.cols[col + i + 1] -= 1
 
     def add_vertical_ship(self, row, col, size):
-        self.ships[size - 1] -= 1
+        if row + size > 10:
+            raise InvalidShip
+
+        if self.cols[col] < size:
+            raise InvalidShip
         self.cols[col] -= size
 
+        for i in range(row, row + size):
+            if not self.rows[i]:
+                raise InvalidShip
+            self.rows[i] -= 1
+
+        self.ships[size - 1] -= 1
+
         self.add_top_cell(row, col)
-        self.rows[row] -= 1
 
         # add middle
         i = 0
         for i in range(1, size - 1):
             self.add_middle_vertical_cell(row + i, col)
-            self.rows[row + i] -= 1
 
         self.add_bottom_cell(row + i + 1, col)
-        self.rows[row + i + 1] -= 1
 
     def add_single_ship(self, row, col):
-        self.ships[0] -= 1
-        self.cols[col] -= 1
+        if not self.rows[row] or not self.cols[col]:
+            raise InvalidShip
+
         self.rows[row] -= 1
+        self.cols[col] -= 1
+        self.ships[0] -= 1
 
         self.add_center_cell(row, col)
 
@@ -126,7 +230,6 @@ class Board:
     def add_middle_horizontal_cell(self, row, col):
         if not self.is_valid_horizontal_middle_cell(row, col):
             raise InvalidCell
-
         self.table[row][col] = "m"
         self.set_cell(row - 1, col + 1, "w")
         self.set_cell(row + 1, col + 1, "w")
@@ -168,7 +271,6 @@ class Board:
     def add_center_cell(self, row, col):
         if not self.is_valid_center_cell(row, col):
             raise InvalidCell
-
         self.table[row][col] = "c"
         self.set_horizontal(row, col, "w")
         self.set_vertical(row, col, "w")
@@ -317,8 +419,14 @@ class Board:
                 self.get_value(row + 1, col + 1), \
                 self.get_value(row - 1, col + 1)
 
-    def __str__(self):
-        ...
+    def board_copy(self):
+        table = numpy.copy(self.table)
+        rows = self.rows.copy()
+        cols = self.cols.copy()
+        ships = self.ships.copy()
+        hints = self.hints.copy()
+
+        return Board(table, rows, cols, hints, ships, len(self.hints) != 0)
 
     @classmethod
     def parse_instance(cls):
@@ -352,7 +460,10 @@ class Board:
                 else:
                     hints.append((x, y, letter))
 
-        return cls(table, rows, cols, hints, ships)
+        return cls(table, rows, cols, hints, ships, len(hints) != 0)
+
+    def __str__(self):
+        return "\n".join("".join(cell for cell in row) for row in self.table)
 
 
 class Bimaru(Problem):
@@ -364,35 +475,32 @@ class Bimaru(Problem):
     def actions(self, state: BimaruState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
-        # TODO
-        pass
+        if not state.board.is_valid:
+            return []
+
+        actions = state.board.get_valid_actions()
+        return actions
 
     def result(self, state: BimaruState, action):
         """Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        # TODO
-        pass
+        return BimaruState(state.board.action(*action))
 
     def goal_test(self, state: BimaruState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas de acordo com as regras do problema."""
-        # TODO
-        pass
+        return state.board.is_complete()
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
-        # TODO
-        pass
-
-    # TODO: outros metodos da classe
-
+        return node.state.board.ships[0] + node.state.board.ships[0]*2 + node.state.board.ships[0]*3 + node.state.board.ships[0]*4
 
 if __name__ == "__main__":
     board = Board.parse_instance()
     problem = Bimaru(board)
-    while 1:
-        continue
+    goal = breadth_first_tree_search(problem)
+    print(goal)
     a = 1
